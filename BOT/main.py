641d5e70f1cc98fd2cd4ca6d2fed2227d741daca
Shahriyor Ibragimov <magmainyourvein@gmail.com>
2026-01-14 23:44:24 +0500

    commit

--- BOT/main.py
+++ BOT/main.py
@@ -1,16 +1,31 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from aiogram.types import WebAppInfo
+from dotenv import load_dotenv
+import asyncio
+import os
 
-bot = Bot(token="BOT_TOKEN")
-dp = Dispatcher()
+load_dotenv()
 
-@dp.message_handler(commands=["start"])
-async def start(msg: types.Message):
+DP = Dispatcher()
+
+
+@DP.message(Command("start"))
+async def command_start_handler(message: types.Message) -> None:
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(
         types.KeyboardButton(
             text="🎓 Open Student App",
-            web_app=WebAppInfo(url="https://your-react-app.com")
+            web_app=WebAppInfo(url="https://chipper-zuccutto-0a2279.netlify.app/")
         )
     )
-    await msg.answer("Welcome!", reply_markup=keyboard)
+    await message.answer("Welcome!", reply_markup=keyboard)
+
+
+async def main() -> None:
+    BOT = Bot(token=os.getenv("API"))
+    await DP.start_polling(BOT)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
